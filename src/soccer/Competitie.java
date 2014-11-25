989b4ab06ac80f833b977c041c0524f29d1a9d7d
Nils <Nils@TNHPC>
2014-11-25 14:53:17 +0100

    Goals + playgame

--- src/soccer/Competitie.java
+++ src/soccer/Competitie.java
@@ -6,6 +6,7 @@
 package soccer;
 
 import java.util.ArrayList;
+import java.util.Collections;
 import java.util.Objects;
 
 /**
@@ -89,4 +90,40 @@ public class Competitie {
     public int getScoreOfTeam(Team t){
         return this.wedstrijden.stream().mapToInt(v -> v.scoreOfTeam(t)).sum();
     }
+    /**
+     * Creërt alle wedstrijden voor een competite uit een ArrayList van Teams.
+     * @param team  ArrayList<Team>
+     */ 
+    public static void bouwWedstrijden(ArrayList<Team> team){
+        
+        ArrayList<Team> listA = new ArrayList<>();
+        ArrayList<Team> listB = new ArrayList<>();
+        ArrayList<Wedstrijd> wedstrijdenlijst = new ArrayList<>();
+        
+        
+        for (int i=0; i<(team.size()/2); i++) {
+            listA.add(team.get(i));
+        }
+        for (int p=(team.size()/2); p<team.size(); p++) {
+            listB.add(team.get(p));
+        }
+        for (int x = 0; x<team.size()-2; x++) {
+            for (int j = 0; j<listA.size(); j++) {
+                wedstrijdenlijst.add(new Wedstrijd(listA.get(j), listB.get(j)));
+            }
+
+            for (int q = 0; q<listA.size(); q++){
+                listB.set(0, listA.get(1));
+                for (int k = 1; k<listA.size(); k++) {
+                    Collections.swap(listA, k, k+1);
+                }
+                for (int m = 1; m<listB.size(); m++) {
+                    Collections.swap(listB, m, m-1);
+                }
+                listA.set(listA.size(), listB.get(listB.size()));
+            }
+        }
+        
+        
+    }   
 }
