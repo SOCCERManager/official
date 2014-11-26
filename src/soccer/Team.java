e2e606450580ec07e698c780484dbc4f2d2ad7df
Nils <Nils@TNHPC>
2014-11-26 15:57:45 +0100

    JavaDoc

--- src/soccer/Team.java
+++ src/soccer/Team.java
@@ -1,9 +1,3 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
-
 package soccer;
 
 import java.util.ArrayList;
@@ -11,7 +5,7 @@ import java.util.Objects;
 
 /**
  *
- * @author Youri
+ * Class Team
  */
 public class Team {
     private ArrayList<Speler> spelers;
@@ -22,11 +16,11 @@ public class Team {
 
     /**
      * This constructor sets the private variables spelers, name, opstelling and c.
-     * @param spelers
-     * @param name
-     * @param opstelling
-     * @param c
-     * @param budget
+     * @param spelers De lijst van spelers in het team (ArrayList<Spelers>)
+     * @param name De naam van het team (String)
+     * @param opstelling De opstelling van het team gemaakt door de positie van de spelers (ArrayList<PosPlayer>)
+     * @param c De competitie waarin dit team speelt (Competitie)
+     * @param budget Het budget van het team (int)
      */
     public Team(ArrayList<Speler> spelers, String name, ArrayList<PosPlayer> opstelling, Competitie c, int budget) {
         this.spelers = spelers;
@@ -43,75 +37,83 @@ public class Team {
     public int getBudget(){
         return budget;
     }
-    
+    /**
+     * Set het budget van het team
+     * @param budget nieuw budget van het team
+     */
     public void setBudget(int budget){
         this.budget = budget;
     }
     
     /**
-     * Returns the arraylist which contains all the spelers in a team.
-     * @return
+     * Geeft alle spelers in een team terug
+     * @return een ArrayList van alle spelers in een team
      */
     public ArrayList<Speler> getSpelers() {
         return spelers;
     }
 
     /**
-     *
-     * @param spelers
+     * Set de lijst van spelers in het team
+     * @param spelers de nieuwe lijst van spelers voor het team
      */
     public void setSpelers(ArrayList<Speler> spelers) {
         this.spelers = spelers;
     }
 
     /**
-     * Gets current competitie object
-     * @return 
+     * Get de competitie van het team
+     * @return  de competitie van het team
      */
     public Competitie getCompetitie() {
         return competitie;
     }
 
     /**
-     * Sets current competitie object
-     * @param competitie 
+     * Set de opgegeven competitie als de huidige voor het team
+     * @param competitie de nieuwe competitie voor het team
      */
     public void setCompetitie(Competitie competitie) {
         this.competitie = competitie;
     }
     
     /**
-     * Returns the name of the team.
-     * @return
+     * Get de naam van het team
+     * @return de naam van het team
      */
     public String getName() {
         return name;
     }
 
     /**
-     *
-     * @param name
+     * Set de naam van het team
+     * @param name de nieuwe naam voor het team
      */
     public void setName(String name) {
         this.name = name;
     }
 
     /**
-     * Returns the current opstelling of the team.
-     * @return
+     * Get de huidige opstelling van het team
+     * @return ArrayList<PosPlayer> waarin de opstelling staat van het team
      */
     public ArrayList<PosPlayer> getOpstelling() {
         return opstelling;
     }
 
     /**
-     *
-     * @param opstelling
+     * Set een opgegeven opstelling van het team
+     * @param opstelling de nieuwe osptelling voor het team
      */
     public void setOpstelling(ArrayList<PosPlayer> opstelling) {
         this.opstelling = opstelling;
     }
 
+    /**
+     * Kijkt of Teams aan elkaar gelijk zijn
+     * @param obj het opgegeven team dat wordt vergeleken met het team waarbij deze methode wordt opgeroepen
+     * @return true als de teams gelijk zijn en false als de teams niet gelijk zijn aan elkaar
+     */
     @Override
     public boolean equals(Object obj) {
         if (obj == null) {
@@ -141,25 +143,33 @@ public class Team {
 
     
     /**
-     * Calls the function getScoreOfTeam which returns an integer and consists out of the total amount of points gained.
-     * @return
+     * Roept getScoreOfTeam in competitie aan en vraagt hierbij het totaal aantal punten van het team
+     * @return het aantal punten van een team
      */
     public int getScore(){
         return this.competitie.getScoreOfTeam(this);
     }
     
     /**
-     * Method to be called when playing a game.
+     * Start een spel (start het spel voor alle spelers om te kijken of deze wel/niet beschikbaar zijn enz.
+     * zie speler.playGame();
      */
     public void playGame() {
         for(Speler s: this.spelers)
             s.playGame();
     }
     
+    /**
+     * Kijkt of de opstelling valide is
+     * @return true als de opstelling valide is en false als de opstelling niet valide is
+     */
     public boolean OpstellingIsValide() {
         return this.opstelling.size() == 11 && ((int) this.opstelling.stream().filter(v -> v.isUnavaliableToPlay()).count()) == 0;
     }
     
+    /**
+     * Creëert een opstelling voor een team
+     */
     public void generateOpstelling() {
         
         // To many players injured, lets magialy fix some (:
@@ -187,6 +197,11 @@ public class Team {
         }
     }
     
+    /**
+     * Berekent de totale Aanvalsstats van een team door alle Aanvalstats van alle spelers in de opstelling
+     * bij elkaar op te tellen
+     * @return het totaal(int)
+     */
     public int getAanvallendTotaal() {
         if(!this.OpstellingIsValide())
             this.generateOpstelling();
@@ -194,12 +209,23 @@ public class Team {
         return this.opstelling.stream().mapToInt(v -> v.getAanvallend()).sum();
     }
     
+    /**
+     * Berekent de totale Verdedigingsstats van een team door alle Verdedigingstats van alle spelers in de opstelling
+     * bij elkaar op te tellen
+     * @return het totaal (int)
+     */
     public int getVerdedigingsTotaal() {
         if(!this.OpstellingIsValide())
             this.generateOpstelling();
         
         return this.opstelling.stream().mapToInt(v -> v.getVerdedigend()).sum();
     }
+    
+    /**
+     * Berekent de totale Uithoudingsstats van een team door alle Uithoudingsstats van alle spelers in de opstelling
+     * bij elkaar op te tellen
+     * @return het totaal (int)
+     */
     public int getUithoudingsTotaal() {
         if(!this.OpstellingIsValide())
             this.generateOpstelling();
@@ -207,7 +233,10 @@ public class Team {
         return this.opstelling.stream().mapToInt(v -> v.getUithoudingsvermogen()).sum();
     }
     
-    
+    /**
+     * Telt alle stats van een team bij elkaar op (aanvallend + verdedigend + uithoudingsvermogen)
+     * @return het totaal van alle stats voor een team
+     */
     public int getTotalStat() {
         return this.getAanvallendTotaal() + this.getVerdedigingsTotaal() + this.getUithoudingsTotaal();
     }
