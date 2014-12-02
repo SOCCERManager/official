db6d9c279154480d052edbf4e3e2b98b5c1ea886
codesalad <codesalad@codesalad-Inspiron-7520>
2014-12-02 10:21:19 +0100

    beginnetje aan gui ^_^

---
+++ src/gui/GUI.java
@@ -0,0 +1,42 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package gui;
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.image.ImageView;
+import javafx.stage.Stage;
+
+/**
+ *
+ * @author codesalad
+ */
+public class GUI extends Application {
+    
+    @Override
+    public void start(Stage stage) throws Exception {
+        Parent root = FXMLLoader.load(getClass().getResource("main.fxml"));
+        root.setId("AnchorPane");
+        
+        
+        Scene scene = new Scene(root);
+        scene.getStylesheets().addAll(this.getClass().getResource("main.css").toExternalForm());
+        
+        stage.setFullScreen(true);
+        stage.setScene(scene);
+        stage.show();
+    }
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        launch(args);
+    }
+    
+}
