aaa177eea736acc292c8d490b5e02555d754a16b
codesalad <codesalad@codesalad-Inspiron-7520>
2014-12-10 12:06:48 +0100

    load screen

--- src/gui/controller/loadscreenController.java
+++ src/gui/controller/loadscreenController.java
@@ -15,13 +15,15 @@ import javafx.event.ActionEvent;
 import javafx.event.EventHandler;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javafx.geometry.Pos;
 import javafx.scene.Scene;
 import javafx.scene.control.TextField;
 import soccer.Competitie;
 import javafx.scene.control.Button;
+import javafx.scene.control.Label;
 import javafx.scene.layout.AnchorPane;
 import javafx.scene.layout.GridPane;
-import javafx.scene.layout.StackPane;
+import javafx.stage.Stage;
 
 /**
  * FXML Controller class
@@ -31,23 +33,38 @@ import javafx.scene.layout.StackPane;
 public class loadscreenController implements Initializable {
     @FXML
     private AnchorPane loadAnchorPane;
+    private GridPane loadGameGrid;
+    private GridPane gameGrid = new GridPane();
     private MainApp mainApp;
+    private int i = 0;
     /**
      * Initializes the controller class.
      */
     @Override
     public void initialize(URL url, ResourceBundle rb) {
-       Button button1 = new Button();
-       button1.setText("Test");
-       button1.setOnAction(new EventHandler<ActionEvent>() {
-           @Override
-           public void handle(ActionEvent event){
-               System.out.println("TESTTESTEST");
-           }
-       });
-       loadAnchorPane.getChildren().add(button1);
-       
-    }  
+        soccer.ReadFolder.Read();
+        while(i<soccer.ReadFolder.getAantalSaveGames()) {
+            //System.out.println("size is:" +soccer.ReadFolder.Read().size());
+            addGames(soccer.ReadFolder.Read().get(i));
+        }
+        loadAnchorPane.getChildren().add(gameGrid);
+    }
+    
+    private void addGames(String name) {
+        i++;
+        final Button temp = new Button(name);
+        final int numButton= i;
+        temp.setId("" + i);
+        temp.setOnAction(new EventHandler<ActionEvent>() {
+            @Override
+            public void handle(ActionEvent e) {
+                System.out.println("LAAD SPEL: " + name);
+            }
+        });
+        gameGrid.add(temp, 1, i);
+    }
+    
+    
     
     @FXML
     private void handleBackButton(){
