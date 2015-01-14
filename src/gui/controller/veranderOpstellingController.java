b81eba65032575997084dd2e2953223d4eaa4a0e
floris <floris@Computtol>
2015-01-14 22:05:04 +0100

    bugfix

--- src/gui/controller/veranderOpstellingController.java
+++ src/gui/controller/veranderOpstellingController.java
@@ -5,6 +5,7 @@
  */
 package gui.controller;
 
+import gui.MainApp;
 import java.io.File;
 import java.net.URL;
 import java.util.ArrayList;
@@ -21,6 +22,8 @@ import javafx.event.EventType;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.geometry.Pos;
+import javafx.scene.ImageCursor;
+import javafx.scene.Node;
 import javafx.scene.control.Button;
 import javafx.scene.control.ContentDisplay;
 import javafx.scene.control.Label;
@@ -33,6 +36,8 @@ import javafx.scene.control.TableView;
 import javafx.scene.control.TextArea;
 import javafx.scene.control.cell.ProgressBarTableCell;
 import javafx.scene.control.cell.PropertyValueFactory;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
 import javafx.scene.input.ClipboardContent;
 import javafx.scene.input.DataFormat;
 import javafx.scene.input.DragEvent;
@@ -216,19 +221,19 @@ public class veranderOpstellingController implements Initializable {
     @FXML
     private AnchorPane mainOpstellingPane;
     @FXML
-    private TableView<PosPlayer> teamTable;
+    private TableView<Speler> teamTable;
     @FXML
-    private TableColumn<PosPlayer, String> filteredTeamColumn;
+    private TableColumn<Speler, String> filteredTeamColumn;
     @FXML
-    private TableColumn<PosPlayer, String> filteredPositieColumn;
+    private TableColumn<Speler, String> filteredPositieColumn;
     @FXML
-    private TableColumn<PosPlayer, Double> filteredAanvalColumn;
+    private TableColumn<Speler, Double> filteredAanvalColumn;
     @FXML
-    private TableColumn<PosPlayer, Double> filteredStaminaColumn;
+    private TableColumn<Speler, Double> filteredStaminaColumn;
     @FXML
-    private TableColumn<PosPlayer, Double> filteredVerdedigingColumn;
+    private TableColumn<Speler, Double> filteredVerdedigingColumn;
     @FXML
-    private TableColumn<PosPlayer, String> filteredStatusColumn;
+    private TableColumn<Speler, String> filteredStatusColumn;
     @FXML
     private AnchorPane titlePane;
     @FXML
@@ -253,20 +258,26 @@ public class veranderOpstellingController implements Initializable {
     private Team userteam = mainhubController.originalteamlist.get(Competitie.getCompetitie().getUserindex()); 
     private ArrayList<Speler> teamSpelers = new ArrayList<Speler>(userteam.getSpelers());
     private ArrayList<PosPlayer> opstelling = new ArrayList<PosPlayer>(userteam.getOpstelling());
+    
     /**
      * Initializes the controller class.
      */
     @Override
     public void initialize(URL url, ResourceBundle rb) {
+        
+        Image image = new Image(getClass().getResourceAsStream("/gui/view/shirt.png"));
+        
         for(int i = 0; i < opstelling.size(); i++)
             System.out.println(opstelling.get(i).getLoc());
         drawTeam();
         drawOpstelling();
         
         /** DRAG AND DROP **/
+        
         teamTable.setOnDragDetected(new EventHandler<MouseEvent>() { //drag
             @Override
             public void handle(MouseEvent event) {
+               
                 // drag was detected, start drag-and-drop gesture
                 String selected = teamTable.getSelectionModel().getSelectedItem().getNaam();
                 System.out.println(selected);
@@ -840,19 +851,18 @@ public class veranderOpstellingController implements Initializable {
     /** EINDE DRAG AND DROP **/
     
     public void drawTeam() {
-        ObservableList<PosPlayer>filteredTeamList = FXCollections.observableArrayList();
-        ArrayList<PosPlayer> allPosPlayers = new ArrayList<PosPlayer>();
-        ArrayList<PosPlayer> filteredTeam = new ArrayList<PosPlayer>();
+        ObservableList<Speler>filteredTeamList = FXCollections.observableArrayList();
+        ArrayList<Speler> allPosPlayers = new ArrayList<Speler>();
+        ArrayList<Speler> filteredTeam = new ArrayList<Speler>();
         filteredTeamList.clear();
-        for(int i=0; i<teamSpelers.size(); i++){
-            allPosPlayers.add(new PosPlayer(teamSpelers.get(i), teamSpelers.get(i).getType()));
+        
+        for(int i = 0; i < userteam.getOpstelling().size(); i++)
+            allPosPlayers.add(userteam.getOpstelling().get(i).getSpeler());
+        for(int i = 0; i < userteam.getSpelers().size(); i++){
+            if(!allPosPlayers.contains(userteam.getSpelers().get(i)))
+                filteredTeam.add(userteam.getSpelers().get(i));
         }
         
-        for(int i=0; i<allPosPlayers.size(); i++){
-            if(opstelling.contains(allPosPlayers.get(i)) == false){
-                filteredTeam.add(allPosPlayers.get(i));
-            }
-        }
         
         filteredTeamList.addAll(filteredTeam);
         filteredTeamColumn.setCellValueFactory(
@@ -863,17 +873,17 @@ public class veranderOpstellingController implements Initializable {
         filteredAanvalColumn.setCellValueFactory(
                 cellData -> new SimpleDoubleProperty(new Double(cellData.getValue().getAanvallend() * 0.01)).asObject());
         filteredAanvalColumn.setCellFactory(
-                ProgressBarTableCell.<PosPlayer> forTableColumn());
+                ProgressBarTableCell.<Speler> forTableColumn());
         
         filteredStaminaColumn.setCellValueFactory(
                 cellData -> new SimpleDoubleProperty(new Double(cellData.getValue().getUithoudingsvermogen()* 0.01)).asObject());
         filteredStaminaColumn.setCellFactory(
-                ProgressBarTableCell.<PosPlayer> forTableColumn());
+                ProgressBarTableCell.<Speler> forTableColumn());
         
         filteredVerdedigingColumn.setCellValueFactory(
                 cellData -> new SimpleDoubleProperty(new Double(cellData.getValue().getVerdedigend()* 0.01)).asObject());
         filteredVerdedigingColumn.setCellFactory(
-                ProgressBarTableCell.<PosPlayer> forTableColumn());
+                ProgressBarTableCell.<Speler> forTableColumn());
         
         filteredStatusColumn.setCellValueFactory(
             cellData -> new SimpleStringProperty(""+cellData.getValue().getStatus()));
